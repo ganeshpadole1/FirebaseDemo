ab5af8ae32e79aed91571d98e9a572d7991039d8
ganesh padole <ganeshpadole55@gmail.com>
2020-05-23 23:50:10 +0530

    Implemented dismiss keyboard functionality.

---
+++ FirebaseDemo/Extensions/DismissKeyboardExtension.swift
@@ -0,0 +1,22 @@
+//
+//  DismissKeyboardExtension.swift
+//  FirebaseDemo
+//
+//  Created by ganesh padole on 23/05/20.
+//  Copyright © 2020 ganesh padole. All rights reserved.
+//
+
+import UIKit
+
+// Put this piece of code anywhere you like
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
