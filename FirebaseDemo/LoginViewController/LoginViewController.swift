9ef67072926c0318d6e7c57e11268de0014b2605
ganesh padole <ganeshpadole55@gmail.com>
2020-05-23 21:02:45 +0530

    Create User Interface for login

---
+++ FirebaseDemo/LoginViewController/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginViewController.swift
+//  FirebaseDemo
+//
+//  Created by ganesh padole on 23/05/20.
+//  Copyright © 2020 ganesh padole. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var createAccountButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginButtonClicked(_ sender: UIButton) {
+    }
+    
+    @IBAction func createAccountButtonClicked(_ sender: UIButton) {
+    }
+    
+}
